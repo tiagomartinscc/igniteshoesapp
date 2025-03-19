ae5d75f6f65c193f1a67c9f1514ce9394ce9adaf
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-19 16:36:07 -0300

    install onesignal

--- App.tsx
+++ App.tsx
@@ -9,6 +9,10 @@ import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
 
+import { OneSignal } from 'react-native-onesignal'
+
+OneSignal.initialize("99c87f1c-4c7b-4384-bdef-1f5b97e75e50")
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
