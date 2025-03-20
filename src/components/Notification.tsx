0174c3521487d67ab9accd764b0e7a3ca5a7f234
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-20 10:49:39 -0300

    redirect user to show the product

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,37 +1,58 @@
-import { HStack, Text, IconButton, CloseIcon, Icon } from 'native-base';
+import { HStack, Text, IconButton, CloseIcon, Icon, Pressable } from 'native-base';
 import { Ionicons } from '@expo/vector-icons';
+import { OSNotification } from 'react-native-onesignal';
+import { useNavigation } from '@react-navigation/native';
 
 type Props = {
-  title: string;
+  data: OSNotification;
   onClose: () => void;
 }
 
-export function Notification({ title, onClose }: Props) {
+type AdditionalDataProps = {
+  route?: string
+  product_id?: string
+}
+
+export function Notification({ data, onClose }: Props) {
+  const { navigate } = useNavigation()
+  function handleOnePress() {
+    const { route, product_id } = data.additionalData as AdditionalDataProps
+
+    if (route === 'details' && product_id) {
+      navigate('details', {productId: product_id})
+      onClose()
+    }
+  }
+
   return (
-    <HStack 
-      w="full" 
+    <Pressable 
+      w="full"  
       p={4} 
-      pt={12}
-      justifyContent="space-between" 
-      alignItems="center" 
+      pt={12} 
       bgColor="gray.200"
       position="absolute"
       top={0}
+      onPress={handleOnePress}
     >
-        <Icon as={Ionicons} name="notifications-outline" size={5} color="black" mr={2}/>
+      <HStack 
+        justifyContent="space-between" 
+        alignItems="center" 
+      >
+          <Icon as={Ionicons} name="notifications-outline" size={5} color="black" mr={2}/>
 
-        <Text fontSize="md" color="black" flex={1}>
-          {title}
-        </Text>
+          <Text fontSize="md" color="black" flex={1}>
+            {data.title}
+          </Text>
 
-      <IconButton 
-        variant="unstyled" 
-        _focus={{ borderWidth: 0 }} 
-        icon={<CloseIcon size="3" />} 
-        _icon={{ color: "coolGray.600"}} 
-        color="black"
-        onPress={onClose}
-      />
-    </HStack>
+        <IconButton 
+          variant="unstyled" 
+          _focus={{ borderWidth: 0 }} 
+          icon={<CloseIcon size="3" />} 
+          _icon={{ color: "coolGray.600"}} 
+          color="black"
+          onPress={onClose}
+        />
+      </HStack>
+    </Pressable>
   );
 }
