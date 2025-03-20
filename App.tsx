1cf9fcbb7ec4bce046ca2e8a0a139f56aa9e87af
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-20 09:37:03 -0300

    create and remove tags

--- App.tsx
+++ App.tsx
@@ -10,12 +10,15 @@ import { Loading } from './src/components/Loading';
 import { CartContextProvider } from './src/contexts/CartContext';
 
 import { OneSignal } from 'react-native-onesignal'
+import { tagUserEmailCreate } from './src/notifications/notificationsTags';
 
 OneSignal.initialize("99c87f1c-4c7b-4384-bdef-1f5b97e75e50")
 OneSignal.Notifications.requestPermission(true)
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  tagUserEmailCreate("tiagomartinscc@hotmail.com")
 
   return (
     <NativeBaseProvider theme={THEME}>
