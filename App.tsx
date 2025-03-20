e9c9385be39327b2f98e585c73e93e407aea4c40
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-20 10:57:27 -0300

    handle click notification

--- App.tsx
+++ App.tsx
@@ -9,8 +9,9 @@ import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
 
-import { OneSignal } from 'react-native-onesignal'
+import { NotificationClickEvent, OneSignal } from 'react-native-onesignal'
 import { tagUserInfoCreate } from './src/notifications/notificationsTags';
+import { useEffect } from 'react';
 
 OneSignal.initialize("99c87f1c-4c7b-4384-bdef-1f5b97e75e50")
 OneSignal.Notifications.requestPermission(true)
@@ -19,6 +20,16 @@ export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   tagUserInfoCreate()
+
+  useEffect(() => {
+    const handleNotificationClick = (event: NotificationClickEvent): void => {
+      console.log(event)
+    }
+
+    OneSignal.Notifications.addEventListener('click', handleNotificationClick)
+
+    return () => OneSignal.Notifications.removeEventListener('click', handleNotificationClick)
+  }, [])
 
   return (
     <NativeBaseProvider theme={THEME}>
